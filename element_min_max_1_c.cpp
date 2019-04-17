769b350845ce0877986aadd01c2a5f438b0d2bbf
CrazyOri <mojamama007@o2.pl>
2019-04-17 10:26:18 +0200

    elminmax

--- element_min_max_1_c.cpp
+++ element_min_max_1_c.cpp
@@ -2,7 +2,7 @@
 //uwaga: elementy tablicy w C numerowane sa od 0
 //       zatem nasza tablica ma indeksy od 0 do n-1
 //       a[0], a[1], ..., a[n-1]
-//www.algorytm.org
+//inpg931
 
 #include <iostream>
 using namespace std;
